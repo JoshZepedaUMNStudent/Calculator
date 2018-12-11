20c5dc56b55a0421d20ab7f83314af4e1ce055d3
JZStudent <joshzepeda.student@gmail.com>
2018-12-10 20:01:03 -0600

    updated App.css and App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import display from "./display";
+import buttonPanel from "./buttonPanel";
+import calculate from "../logic/calclate";
+import "./App.css";
 
-class App extends Component {
+class App extends React.components {
+  constructor(props) {
+    super(props):
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="component-app">
+        <display value={this.state.next || this.state.total || "0"} />
+        <buttonPanel clickHandler={this.handleClick} />
       </div>
-    );
+    )
   }
 }
-
-export default App;
