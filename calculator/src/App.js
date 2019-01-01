409e863bfc0fe72c3f9830725dfff53729f2c2e9
JZStudent <joshzepeda.student@gmail.com>
2018-12-31 19:41:47 -0600

    finalized

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Answers from "./components/Answers";
+import KeyPad from "./components/KeyPad";
+
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate()
+    }
+
+    else if(button === "C") {
+      this.reset()
+    }
+
+    else if(button === "CE") {
+      this.backspace()
+    }
+
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    try {
+      this.setState({
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (e) {
+      this.setState.State({
+        result: "error"
+      })
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  }
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="calculator-body">
+          <h1>Simple Calculator</h1>
+          <Answers result={this.state.result}/>
+          <KeyPad onClick={this.onClick}/>
+        </div>
       </div>
-    );
+    )
   }
-}
+
+
+};
 
 export default App;
